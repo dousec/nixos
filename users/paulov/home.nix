6d8bebab3135b7a4e60a449e14fa8b1d05483feb
Paulo Viteri <paulov@dousec.org>
2026-04-17 21:09:05 -0500

    feat(flake): update

--- users/paulov/home.nix
+++ users/paulov/home.nix
@@ -1,7 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.username = "paulov";
   programs.zsh.enable = true;
   # programs.git.enable = true; it must be enabled by default on any machine, i expect that..
+  home.packages = with pkgs; [
+    nh
+  ];
   home.stateVersion = "25.05";
 }
