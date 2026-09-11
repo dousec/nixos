9bfa029d52a65b6c2aa2250e7edb5ff469dd0005
Paulo Viteri <undevoted@proton.me>
2026-09-11 01:26:41 -0500

    feat(bifrost): add container to oci

--- systems/maverick/programs/cloudflared.nix
+++ systems/maverick/programs/cloudflared.nix
@@ -7,12 +7,12 @@
         "0a2cf345-ad5c-4c81-82f1-ae6c0b63185d" = {
           credentialsFile = config.sops.secrets."cloudflared/dousec/tunnel/argo_key".path;
           ingress = {
-            "git.dousec.org" = "http://localhost:8086";
-            "cloud.dousec.org" = "http://localhost:8087";
             "attic.dousec.org" = "http://localhost:8081";
+            "cloud.dousec.org" = "http://localhost:8087";
             "dousec.org" = "http://localhost:80";
-            "papers.dousec.org" = "http://localhost:80";
+            "git.dousec.org" = "http://localhost:8086";
             "n8n.dousec.org" = "http://localhost:80";
+            "papers.dousec.org" = "http://localhost:80";
           };
           default = "http_status:404";
         };
@@ -20,6 +20,7 @@
         "10c3b49e-4690-4b4a-b83d-a7f81b8c8549" = {
           credentialsFile = config.sops.secrets."cloudflared/paulov/tunnel/argo_key".path;
           ingress = {
+            "ai.paulov.dev" = "http://localhost:8083";
             "paulov.dev" = "http://localhost:80";
           };
           default = "http_status:404";
