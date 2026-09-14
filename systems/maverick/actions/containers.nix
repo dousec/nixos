eaa52aabc4cb8e4ae5bd8d91caa052e1be4da2f7
Paulo Viteri <undevoted@proton.me>
2026-09-13 20:07:23 -0500

    feat(bifrost): add container and tunel record

--- systems/maverick/actions/containers.nix
+++ systems/maverick/actions/containers.nix
@@ -34,6 +34,15 @@
           ];
           autoStart = true;
         };
+
+        bifrost = {
+          image = "docker.io/maximhq/bifrost";
+          ports = [ "8083:8080" ];
+          volumes = [
+            "/opt/bifrost:/app/data"
+          ];
+          autoStart = true;
+        };
       };
     };
   };
